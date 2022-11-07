dbacb5896295183be87b82b813954792f955b06d
Alexander Valverde Guillen <alexandervalverdeguillen@gmail.com>
2022-11-07 16:18:28 -0600

    Deployment

--- prediction.py
+++ prediction.py
@@ -4,17 +4,17 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 
+model_file = "lar.bin"
+
+with open(model_file, 'rb') as f_in:
+    dv, model = pickle.load(f_in)
+
 def clean(grade, dv, model):
     
     grade = request.get_json()
     X = dv.transform([grade])
     y_pred = model.predict(X)
     return y_pred
-
-model_file = "lar.bin"
-
-with open(model_file, 'rb') as f_in:
-    dv, model = pickle.load(f_in)
 
 app = Flask('prediction')
 @app.route('/prediction', methods=['POST'])
